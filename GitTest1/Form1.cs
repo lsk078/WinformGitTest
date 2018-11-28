f2970d34481f7ffbbc80ecee5055369a3d4420fe
LSK078 <lskt078@gmail.com>
2018-11-28 11:08:08 +0900

    버튼 추가

--- GitTest1/Form1.cs
+++ GitTest1/Form1.cs
@@ -16,5 +16,15 @@ namespace GitTest1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
